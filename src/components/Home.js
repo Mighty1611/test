29d189f72c60e7bb9323e087155c6f08450f6fa5
Mighty1611 <abishailovelson@gmail.com>
2025-07-26 17:34:54 +0530

    added Many Components

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,9 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // components
 import HeroImage from './HeroImage'
+import Grid from './Grid'
+import Thumb from './Thumb'
+import Spinner from './Spinner'
 
 // hook 
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -24,16 +27,29 @@ const Home = () => {
         <>
             {state.results[0] ? (
             <HeroImage 
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
-            // title={`some title`}
-            // text={`some text`}
-            
-            title={state.results[0].original_title}
-            text={state.results[0].overview}
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+                // title={`some title`}
+                // text={`some text`}
+                
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
             />
-            ) : null
-            }
-            Home Page
+            ) : null}
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <Thumb
+                        key={movie.id}
+                        clickable={true}
+                        image={
+                            movie.poster_path 
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        }
+                        movieId = {movie.id}
+                    />
+                ))}
+            </Grid>
+            <Spinner/>
         </>
     )
 };
