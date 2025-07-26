bbe63f16baa7748e90289ea1c79cacb8764398db
Mighty1611 <abishailovelson@gmail.com>
2025-07-26 16:10:15 +0530

    heroImage

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,8 +25,11 @@ const Home = () => {
             {state.results[0] ? (
             <HeroImage 
             image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
-            title='Some title' 
-            text='Some text'
+            // title={`some title`}
+            // text={`some text`}
+            
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
             />
             ) : null
             }
